5c7401a55410ae3e52ba21b972b6ba340a85224a
mohanraj-raja-ai <52619704+mohanraj-raja-ai@users.noreply.github.com>
2019-08-05 22:31:15 +0530

    Update DataStructure_CSharp.cs
    Added Stack

--- DataStructure_CSharp.cs
+++ DataStructure_CSharp.cs
@@ -27,3 +27,67 @@ public static object BinarySearchRecursive(int[] inputArray, int key, int min, i
                     return BinarySearchRecursive(inputArray, key, min, mid - 1);
             }
         }
+
+
+#Stack
+class Program
+    {
+	static int[] stack = new int[100];
+        static int top = -1;
+
+
+        static void Main(string[] args)
+	{
+		 Stack();
+	}
+
+ 	public static void Stack()
+        {
+            int n = 0;
+            Console.WriteLine("Do you want to continue if Yes, Press any number, else press -1");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n != -1)
+            {
+                Console.WriteLine("Enter the choice: (1)PUSH (2)POP");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 1)
+                {
+                    Console.WriteLine("Enter the element to Push");
+                    int element = Convert.ToInt32(Console.ReadLine());
+                    Push(element);
+                }
+
+                else
+                {
+                    Console.WriteLine("Enter the element to POP");
+                    int element = Convert.ToInt32(Console.ReadLine());
+                    Pop();
+                }
+                Console.WriteLine("Do you want to continue if Yes, Press any number, else press -1");
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        public static object Push(int element)
+        {
+            if (top == 99)
+                return "stack is full";
+            else
+            {
+                top++;
+                stack[top] = element;
+                return "Pushed to stack";
+            }
+
+        }
+
+        public static object Pop()
+        {
+            if (top == -1)
+                return "stack is empty";
+            else
+            {
+                return stack[top--];
+            }
+        }
+}
